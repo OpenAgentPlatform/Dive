f8bc35dedc0386cdf882ec4dce0ba5148a7f32e4
Johnny <johnaa0219@gmail.com>
2025-04-23 11:43:10 +0800

    Add handling for the case when localStorage is light

--- src/atoms/themeState.ts
+++ src/atoms/themeState.ts
@@ -1,9 +1,12 @@
 import { atom } from "jotai"
 
-export type ThemeType = "system" | "dark"
+export type ThemeType = "system" | "dark" | "light"
 
 export const systemThemeAtom = atom<"light" | "dark">(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
-export const userThemeAtom = atom<ThemeType>(localStorage.getItem("theme") as ThemeType || "system")
+export const userThemeAtom = atom<ThemeType>((() => {
+  const savedTheme = localStorage.getItem("theme") as ThemeType
+  return savedTheme === "light" ? "system" : (savedTheme || "system")
+})())
 
 export const themeAtom = atom<ThemeType>(get => {
   const userTheme = get(userThemeAtom)
