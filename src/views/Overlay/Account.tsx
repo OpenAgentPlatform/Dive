1374dd14785a0bd7a856d99311492a3fe2f810f0
Johnny <johnaa0219@gmail.com>
2025-09-12 15:17:21 +0800

    feat: move sidebar sidemenu into setting, account page, updateButton UI

--- src/views/Overlay/Account.tsx
+++ src/views/Overlay/Account.tsx
@@ -117,7 +117,7 @@ const Account = () => {
               <div className="section-title">{t("system.planSectionTitle")}</div>
               <Tooltip content={t("system.usageAnalytics")}>
                 <button className="analytics-btn" onClick={() => openUrl(USAGE_ANALYTICS_URL)}>
-                  <svg className="analytics-icon" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
                     <path d="M3 5V17.5C3 18.3284 3.67157 19 4.5 19H18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                     <path d="M3 11L7.5 7.5L10.5 11.5L16.5 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     <path d="M7 13L7 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
@@ -125,6 +125,9 @@ const Account = () => {
                     <path d="M16 12L16 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                   </svg>
                   <span>{t("system.usageAnalyticsTitle")}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 17 16" fill="none">
+                    <path d="M3.83333 14C3.46667 14 3.15278 13.8694 2.89167 13.6083C2.63056 13.3472 2.5 13.0333 2.5 12.6667V3.33333C2.5 2.96667 2.63056 2.65278 2.89167 2.39167C3.15278 2.13056 3.46667 2 3.83333 2H7.83333C8.02222 2 8.18056 2.06389 8.30833 2.19167C8.43611 2.31944 8.5 2.47778 8.5 2.66667C8.5 2.85556 8.43611 3.01389 8.30833 3.14167C8.18056 3.26944 8.02222 3.33333 7.83333 3.33333H3.83333V12.6667H13.1667V8.66667C13.1667 8.47778 13.2306 8.31944 13.3583 8.19167C13.4861 8.06389 13.6444 8 13.8333 8C14.0222 8 14.1806 8.06389 14.3083 8.19167C14.4361 8.31944 14.5 8.47778 14.5 8.66667V12.6667C14.5 13.0333 14.3694 13.3472 14.1083 13.6083C13.8472 13.8694 13.5333 14 13.1667 14H3.83333ZM13.1667 4.26667L7.43333 10C7.31111 10.1222 7.15556 10.1833 6.96667 10.1833C6.77778 10.1833 6.62222 10.1222 6.5 10C6.37778 9.87778 6.31667 9.72222 6.31667 9.53333C6.31667 9.34444 6.37778 9.18889 6.5 9.06667L12.2333 3.33333H10.5C10.3111 3.33333 10.1528 3.26944 10.025 3.14167C9.89722 3.01389 9.83333 2.85556 9.83333 2.66667C9.83333 2.47778 9.89722 2.31944 10.025 2.19167C10.1528 2.06389 10.3111 2 10.5 2H13.8333C14.0222 2 14.1806 2.06389 14.3083 2.19167C14.4361 2.31944 14.5 2.47778 14.5 2.66667V6C14.5 6.18889 14.4361 6.34722 14.3083 6.475C14.1806 6.60278 14.0222 6.66667 13.8333 6.66667C13.6444 6.66667 13.4861 6.60278 13.3583 6.475C13.2306 6.34722 13.1667 6.18889 13.1667 6V4.26667Z" fill="currentColor"/>
+                  </svg>
                 </button>
               </Tooltip>
             </div>
@@ -177,19 +180,6 @@ const Account = () => {
                       <div className="usage-color-circle mcp"></div>
                       MCP <span className="usage-details-value">{countFormat(oapUsage?.mcp ?? 0)}</span>
                       / All token <span className="usage-details-value">{countFormat(oapUsage?.limit ?? 0)}</span>
-                      <Tooltip content={t("system.usageWarningHint")}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 22" width="18" height="18">
-                          <g clipPath="url(#ic_information_svg__a)">
-                            <circle cx="11.5" cy="11" r="10.25" stroke="currentColor" strokeWidth="1.5"></circle>
-                            <path fill="currentColor" d="M9.928 13.596h3.181c-.126-2.062 2.516-2.63 2.516-5.173 0-2.01-1.6-3.677-4.223-3.608-2.229.051-4.08 1.288-4.026 3.9h2.714c0-.824.593-1.168 1.222-1.185.593 0 1.258.326 1.222.962-.144 1.942-2.911 2.389-2.606 5.104Zm1.582 3.591c.988 0 1.779-.618 1.779-1.563 0-.963-.791-1.581-1.78-1.581-.97 0-1.76.618-1.76 1.58 0 .946.79 1.565 1.76 1.565Z"></path>
-                          </g>
-                          <defs>
-                            <clipPath id="ic_information_svg__a">
-                              <path fill="currentColor" d="M.5 0h22v22H.5z"></path>
-                            </clipPath>
-                          </defs>
-                        </svg>
-                      </Tooltip>
                     </div>
                   </div>
                 </div>
--- src/views/Overlay/Tools/Popup/CustomEdit.tsx
+++ src/views/Overlay/Tools/Popup/CustomEdit.tsx
@@ -1318,7 +1318,7 @@ const CustomEdit = React.memo(({ _type, _config, _toolName, onDelete, onCancel, 
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
           </svg>
         </Button>
-        Server Config
+        MCP Config
       </div>
       <div className="tool-edit-popup">
         {CustomList}
